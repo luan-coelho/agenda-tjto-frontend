a8c4d3266487b64560c10ce271db581eccdb2d2b
Luan Coêlho <luancoelho.dev@gmail.com>
2024-07-17 22:46:53 -0300

    feat: Criação do Projeto Frontend.

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import "@/styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
