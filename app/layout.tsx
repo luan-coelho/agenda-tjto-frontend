9e532d779356137d51bb0680981eded8caa9ef27
Luan Coêlho <luancoelho.dev@gmail.com>
2024-07-18 22:12:58 -0300

    feat: Iniciando criação de componentes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import { Inter as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
-import { ReactNode } from "react";
+import { InitialLetterMenu } from "@/components/ui/initial-letter-menu/initial-letter-menu";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -11,10 +11,21 @@ const fontSans = FontSans({
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="pt-br" suppressHydrationWarning>
       <head />
-      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-        <main>{children}</main>
+      <body
+        className={cn(
+          "min-h-screen h-screen bg-background flex items-center justify-center font-sans antialiased",
+          fontSans.variable,
+        )}>
+        <div className="w-5/6 h-5/6 border-gray-300 border-2 rounded-xl p-4">
+          <div className="flex flex-col">
+            <div className="flex items-center justify-center">
+              <InitialLetterMenu />
+            </div>
+            <main>{children}</main>
+          </div>
+        </div>
       </body>
     </html>
   );
